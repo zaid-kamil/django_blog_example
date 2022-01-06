35f4d6727a5b2a7fe08752971c5ea0bf75e55613
zaid kamil <xaidmetamorphos@gmail.com>
2022-01-06 20:11:28 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#hz#%6&yb@l-_r$i-%=p)57l*kyhos(@ta(rmt$tea!er7w2*%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xaid.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
